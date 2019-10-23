94428e82cd8ffb45aaebda82e52b9e47080c7d8f
Nenișcă Maria <50595944+NeniscaMaria@users.noreply.github.com>
2019-10-23 22:09:36 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 /*
 BUGS:
 ->see why a transer is logged in 2 times if read from file. When running with debugger, it has no problem
-->at additional info, when clicking for a second add info, fatal error
 
 NOTES:
 ->the app opens in the daily page----------------------------------------------------------------------------------------------------------------
@@ -51,36 +50,3 @@ int main(int argc, char *argv[])
 	window->show();
 	return a.exec();
 }
-//#include <qtwidgets/qapplication>
-//#include <qtwidgets/qmainwindow>
-//#include <qtcharts/qlineseries>
-//#include <qtcharts/qpieseries>
-//#include <qtcharts/qpieslice>
-//
-//QT_CHARTS_USE_NAMESPACE
-//
-//int main(int argc, char *argv[])
-//{
-//	QApplication a(argc, argv);
-//
-//	QLineSeries *series = new QLineSeries();
-//	series->append(0, 6);
-//	series->append(2, 4);
-//	series->append(3, 8);
-//	series->append(7, 4);
-//	series->append(10, 5);
-//	//*series << QPointF(11, 1) << QPointF(13, 3) << QPointF(17, 6) << QPointF(18, 3) << QPointF(20, 2);
-//	QChart *chart = new QChart();
-//	chart->setAnimationOptions(QChart::AllAnimations);
-//	chart->legend()->hide();
-//	chart->addSeries(series);
-//	chart->createDefaultAxes();
-//	chart->setTitle("Simple line chart example");
-//	QChartView *chartView = new QChartView(chart);
-//	chartView->setRenderHint(QPainter::Antialiasing);
-//	QMainWindow window;
-//	window.setCentralWidget(chartView);
-//	window.resize(400, 300);
-//	window.show();
-//	return a.exec();
-//}
